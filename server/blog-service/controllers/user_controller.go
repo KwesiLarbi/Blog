537fbca1dff740e03e5ba8641ed0d44b596c14e1
KwesiLarbi <kwesiiilarb@gmail.com>
2023-02-02 15:19:39 -0500

    Updating registration controller

--- server/blog-service/controllers/user_controller.go
+++ server/blog-service/controllers/user_controller.go
@@ -2,27 +2,55 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
-	"log"
 
 	"github.com/KwesiLarbi/blog-service/configs"
 	"github.com/KwesiLarbi/blog-service/models"
 	"github.com/KwesiLarbi/blog-service/responses"
+	"github.com/KwesiLarbi/blog-service/helpers"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
 var validate = validator.New()
 
+// HashPassword is used to encrypt the password before it is stored in the DB
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return string(bytes)
+}
+
+// VerifyPassword checks the input password while verifying it with the password in the DB
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("login or password is incorrect")
+		check = false
+	}
+
+	return check, msg
+}
+
 func Register() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		var user models.User
 		defer cancel()
 
@@ -46,23 +74,50 @@ func Register() gin.HandlerFunc {
 			return
 		}
 
-		newUser := models.User{
-			Id: primitive.NewObjectID(),
-			Name: user.Name,
-			Email: user.Email,
-			Password: user.Password,
-			CreationTime: time.Now(),
-		}
-
-		result, err := userCollection.InsertOne(ctx, newUser)
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
 		if err != nil {
+			log.Panic(err)
 			c.JSON(http.StatusInternalServerError, responses.UserResponse{
 				Status: http.StatusInternalServerError,
 				Message: "error",
-				Data: map[string]interface{}{"data": err.Error()},
+				Data: map[string]interface{}{"data": "Error occured whle checking for email"},
 			})
 			return
 		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		if count  > 0 {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{
+				Status: http.StatusInternalServerError,
+				Message: "error",
+				Data: map[string]interface{}{"data": "This email already exists!"},
+			})
+			return
+		}
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserId = user.ID.Hex()
+		token, refreshToken, _ := GenerateAllTokens(*user.Email, *user.FirstName, *user.LastName, user.UserId)
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+
+		result, err := userCollection.InsertOne(ctx, user)
+		if err != nil {
+			msg := fmt.Sprintf("User item was not created!")
+
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{
+				Status: http.StatusInternalServerError,
+				Message: "error",
+				Data: map[string]interface{}{"data": msg},
+			})
+			return
+		}
+		defer cancel()
 
 		c.JSON(http.StatusCreated, responses.UserResponse{
 			Status: http.StatusCreated,
